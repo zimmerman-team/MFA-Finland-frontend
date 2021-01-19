cff1e4da1f590f21337ad2255f8ab5d6cc616f79
Jim Fung <jim@zimmermanzimmerman.nl>
2021-01-19 16:00:44 +0100

    feat: multilang

--- src/app/components/AppBar/index.tsx
+++ src/app/components/AppBar/index.tsx
@@ -1,20 +1,22 @@
+// @ts-nocheck
 import React from "react";
 import MUIAppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
-// import LogoMFA from "app/assets/mfa_logo.svg";
 import theme, { PrimaryColor, SecondaryColor } from "app/theme";
 import { NavLink } from "react-router-dom";
 import { useRecoilState } from "recoil";
-import { drawerAtom } from "app/state/recoil/atoms";
+import { drawerAtom, languageAtom } from "app/state/recoil/atoms";
 import { css } from "styled-components/macro";
 import { MfaLogo } from "app/assets/mfa_logo";
 import LanguageIcon from "@material-ui/icons/Language";
 import SearchIcon from "@material-ui/icons/Search";
+import { Popover } from "@material-ui/core";
 
 export function AppBar() {
   const [drawerState, setDrawerState] = useRecoilState(drawerAtom);
+  const [currentLanguage, setLanguage] = useRecoilState(languageAtom);
 
   const toggleDrawer = (open: boolean) => (
     event: React.KeyboardEvent | React.MouseEvent
@@ -40,6 +42,21 @@ export function AppBar() {
     color: ${PrimaryColor[2]};
     margin-left: 15px;
   `;
+
+  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(
+    true
+  );
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
 
   return (
     <React.Fragment>
@@ -70,6 +87,8 @@ export function AppBar() {
             background-color: ${PrimaryColor[0]};
           `}
         >
+          {/* ---------------------------------------------- */}
+          {/* logo */}
           <NavLink
             to="/"
             css={`
@@ -84,6 +103,9 @@ export function AppBar() {
             </IconButton>
             <div css={LogoText}>IATI Portal for MFA Finland</div>
           </NavLink>
+
+          {/* ---------------------------------------------- */}
+          {/* searchfield */}
           <div
             css={`
               display: flex;
@@ -130,7 +152,12 @@ export function AppBar() {
               </div>
             </div>
 
+            {/* ---------------------------------------------- */}
+            {/* lang switch */}
+            {/* todo: move to separate component */}
             <div
+              aria-describedby={id}
+              onClick={handleClick}
               css={`
                 display: flex;
                 flex-direction: column;
@@ -138,6 +165,8 @@ export function AppBar() {
                 justify-content: center;
                 margin-left: 20px;
                 margin-right: 20px;
+                user-select: none;
+                cursor: pointer;
               `}
             >
               <LanguageIcon
@@ -153,13 +182,77 @@ export function AppBar() {
                   line-height: 1;
                   display: flex;
                   align-items: center;
-
                   color: #ffffff;
+                  text-transform: uppercase;
                 `}
               >
-                ENG
+                {currentLanguage}
               </div>
             </div>
+
+            <Popover
+              id={id}
+              open={open}
+              anchorEl={anchorEl}
+              onClose={handleClose}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "center",
+              }}
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "center",
+              }}
+            >
+              <div
+                css={`
+                  display: flex;
+                  flex-direction: column;
+                  background-color: white;
+                  padding: 16px;
+
+                  div {
+                    user-select: none;
+                    cursor: pointer;
+                    text-transform: uppercase;
+                    margin-bottom: 16px;
+
+                    &:hover {
+                      opacity: 0.5;
+                    }
+
+                    &:last-child {
+                      margin-bottom: 0;
+                    }
+                  }
+                `}
+              >
+                <div
+                  onClick={() => {
+                    setLanguage("fi");
+                  }}
+                >
+                  fi
+                </div>
+                <div
+                  onClick={() => {
+                    setLanguage("sv");
+                  }}
+                >
+                  sv
+                </div>
+                <div
+                  onClick={() => {
+                    setLanguage("en");
+                  }}
+                >
+                  en
+                </div>
+              </div>
+            </Popover>
+
+            {/* ---------------------------------------------- */}
+            {/* burger menu */}
             <IconButton
               edge="end"
               color="inherit"
