27cc4926018f3ade865187dc949fdff423fc85ac
Jim Fung <jim@zimmermanzimmerman.nl>
2021-01-25 20:25:27 +0100

    feat: added detail page layouts

---
+++ src/app/modules/detail-modules/sector-detail-module/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import useTitle from "react-use/lib/useTitle";
+import { DetailModuleLayout } from "app/modules/detail-modules/common/layout";
+import { BreadcrumbLinkModel } from "app/components/Breadcrumb/data";
+import { Path, AppName } from "app/const/Path";
+
+const moduleName: string = "Sector Detail Module";
+export const crumbs: BreadcrumbLinkModel[] = [
+  { label: "Homepage", path: Path.home },
+  { label: moduleName },
+];
+
+export function SectorDetailModule() {
+  useTitle(`${AppName} - ${moduleName}`);
+
+  return <DetailModuleLayout label={moduleName} crumbs={crumbs} />;
+}
