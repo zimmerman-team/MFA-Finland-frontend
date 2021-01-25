e7b6b2c6fd42f942de1805b346a690936adcf096
Jim Fung <jim@zimmermanzimmerman.nl>
2021-01-25 20:27:43 +0100

    feat: added routes for created pages

--- src/app/Routes.tsx
+++ src/app/Routes.tsx
@@ -5,9 +5,17 @@ import { Route, Switch } from "react-router-dom";
 import { PageLoader } from "app/modules/common/page-loader";
 import { NoMatchPage } from "app/modules/common/no-match-page";
 
-const VizModule = lazy(() => import("app/modules/viz-module"));
-const AboutModule = lazy(() => import("app/modules/about-module"));
-const LandingModule = lazy(() => import("app/modules/landing-module"));
+import VizModule from "app/modules/viz-module";
+
+import { LandingModule } from "app/modules/landing-module";
+import { RegionDetailModule } from "app/modules/detail-modules/region-detail-module";
+import { CountryDetailModule } from "app/modules/detail-modules/country-detail-module";
+import { ThematicDetailModule } from "app/modules/detail-modules/thematic-detail-module";
+import { OrganisationDetailModule } from "app/modules/detail-modules/organisation-detail-module";
+import { AboutModule } from "app/modules/about-module";
+import { PrivacyModule } from "app/modules/privacy-module";
+import { StatementModule } from "app/modules/statement-module";
+import { FeedbackModule } from "app/modules/feedback-module";
 
 export function ModuleRoutes() {
   return (
@@ -17,8 +25,40 @@ export function ModuleRoutes() {
           <LandingModule />
         </Route>
 
+        <Route exact path="/regions/:region">
+          <RegionDetailModule />
+        </Route>
+
+        <Route exact path="/countries/:country">
+          <CountryDetailModule />
+        </Route>
+
+        <Route exact path="/sectors/:sector">
+          <CountryDetailModule />
+        </Route>
+
+        <Route exact path="/organisations/:organisation">
+          <OrganisationDetailModule />
+        </Route>
+
+        <Route exact path="/thematic/:theme">
+          <ThematicDetailModule />
+        </Route>
+
         <Route exact path="/about">
           <AboutModule />
+        </Route>
+
+        <Route exact path="/privacy">
+          <PrivacyModule />
+        </Route>
+
+        <Route exact path="/statement">
+          <StatementModule />
+        </Route>
+
+        <Route exact path="/feedback">
+          <FeedbackModule />
         </Route>
 
         <Route exact path="/viz/:tab">
