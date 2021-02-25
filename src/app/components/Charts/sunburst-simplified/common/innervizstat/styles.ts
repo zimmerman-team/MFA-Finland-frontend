66af84c89ac7e167eefa911a898c603034ee687e
Jim Fung <jim@zimmerman.team>
2021-02-25 16:08:23 +0100

    chore: removed unused css

--- src/app/components/Charts/sunburst-simplified/common/innervizstat/styles.ts
+++ src/app/components/Charts/sunburst-simplified/common/innervizstat/styles.ts
@@ -13,16 +13,3 @@ export const totalcssSimplified = css`
   flex-direction: column;
   justify-content: center;
 `;
-
-export const backbuttoncssSimplified = css`
-  z-index: 1;
-  color: #fff;
-  cursor: pointer;
-  font-size: 14px;
-  padding: 6px 12px;
-  user-select: none;
-  font-weight: bold;
-  text-align: center;
-  border-radius: 20px;
-  background: ${PrimaryColor[0]};
-`;
