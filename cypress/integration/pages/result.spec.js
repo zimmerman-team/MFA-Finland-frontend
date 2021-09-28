1f54997ca89aab238910e7b91d482c607c7ab3a6
Wei Guo <guow1227@gmail.com>
2021-09-28 15:54:31 +0200

    chore: fixed result text typo

--- cypress/integration/pages/result.spec.js
+++ cypress/integration/pages/result.spec.js
@@ -10,12 +10,12 @@ context("Result", () => {
     cy.wait(10000);
     cy.get('[test-id="main-page-accept"]').click();
     cy.visit("localhost:3000/result");
-    cy.get("h2").contains("Result").should("exist");
+    cy.get("h2").contains("Development cooperation results").should("exist");
   });
 
   it("inpage navigation", () => {
     cy.get(".InPageNavigation___StyledDiv-sc-1nr5zk8-0").should("exist");
-    cy.contains("Result").click();
+    cy.contains("Development cooperation results").click();
     cy.get(".InPageNavigation___StyledArrowUpwardIcon-sc-1nr5zk8-2").click();
     cy.get(".InPageNavigation___StyledArrowDownwardIcon-sc-1nr5zk8-1").click();
   });
