4476603e23465a4ee3cde0293458768643b46118
Stefanos <stefanos@zimmerman.team>
2021-11-24 17:18:15 +0200

    chore: remove jira feedback

--- src/app/third-party-scripts/Scripts.tsx
+++ src/app/third-party-scripts/Scripts.tsx
@@ -3,9 +3,6 @@ import { useScript } from "app/hooks/useScripts";
 
 export const ThirdPartyScripts = () => {
   useScript("/matomo.js");
-  useScript(
-    `https://zimmermanzimmerman.atlassian.net/${process.env.REACT_APP_ISSUE_COLLECTOR_URI}`
-  );
 
   return <></>;
 };
